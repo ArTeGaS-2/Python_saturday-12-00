3eb98de88b7406d51653361451b083e9a764eed2
ArTeGaS-2 <probablytorvik@gmail.com>
2024-10-19 20:00:03 +0300

    new funcs

--- guess.py
+++ guess.py
@@ -1,15 +1,23 @@
 import random # Модуль, дозволяє створювати випадкові числа
 
+attempts = 0 # Додаємо лічильник спроб
+
 # Загадуємо число від 1 до 100
 secret_number = random.randint(1,100)
 # print(secret_number)
 
-# Отримуємо введення від користувача
-guess = int(input("Введіть число від 1 до 100: \n"))
+guess = None # Порожнє значення для старту
 
-if guess < secret_number:
-    print("Загадане число більше!")
-elif guess > secret_number:
-    print("Загадане число меньше!")
-else:
-    print("Вітаю ви виграли!")
+while guess != secret_number: # Цикл повтоюється, доки не вгадано число
+
+    attempts += 1
+
+    # Отримуємо введення від користувача
+    guess = int(input("Введіть число від 1 до 100: \n"))
+
+    if guess < secret_number:
+        print("Загадане число більше!")
+    elif guess > secret_number:
+        print("Загадане число меньше!")
+    else:
+        print(f"Вітаю ви вгадали за {attempts} спроб!")
